e6f2be6a649e1e7d1230c6fbc57d7a1f19cb58e0
Rajendhiran27 <81033046+Rajendhiran27@users.noreply.github.com>
2022-04-07 16:43:35 +0530

    Update register.component.ts

--- angularapp/src/app/register/register.component.ts
+++ angularapp/src/app/register/register.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormArray, FormControl, FormGroup, Validators } from "@angular/forms";
 import { Register } from './register';
+import {RegisterService} from './register.service';
 import { Router } from '@angular/router';
 @Component({
   selector: 'app-register',
@@ -8,25 +8,32 @@ import { Router } from '@angular/router';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-  registrationForm: any;
-  // user = new Register('', '', '', []);
-  isRegistered = false;
-  submitted = false;
-  errorMessage = '';
  hide=true;
-
-  constructor(private router:Router) { }
-  login(){
-    this.router.navigate(['/login']);
-  }
-  ngOnInit(): void {
-    this.registrationForm = new FormGroup({
-      userName: new FormControl(null, [Validators.required, Validators.minLength(5), Validators.maxLength(20)]),
-      email: new FormControl(null, [Validators.required, Validators.email]),
-      password: new FormControl(null, [Validators.required, Validators.minLength(8)]),
-      // roleSelection: this.createRoles(this.roles)
-  });
  
-  }
+register: Register = new Register();
+constructor(private registerService: RegisterService,
+  private router: Router) { }
 
+ngOnInit(): void {
+}
+
+saveRegister(){
+  this.registerService.createRegister(this.register).subscribe( data =>{
+    console.log(data);
+    this.goToEmployeeList();
+  },
+  error => console.log(error));
+}
+// login(){
+//       this.router.navigate(['/signin']);
+//     }
+
+goToEmployeeList(){
+  this.router.navigate(['/signin']);
+}
+
+onSubmit(){
+  console.log(this.register);
+  this.saveRegister();
+}
 }
