5aea75a9db95928e76c74144fe8804c6962ddfca
Rajendhiran27 <81033046+Rajendhiran27@users.noreply.github.com>
2022-04-07 16:35:14 +0530

    Update employee-details.component.ts

--- angularapp/src/app/locker/credential/employee-details/employee-details.component.ts
+++ angularapp/src/app/locker/credential/employee-details/employee-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Employee } from '../employee';
+import { ActivatedRoute } from '@angular/router';
+import { EmployeeService } from '../employee.service';
 
 @Component({
   selector: 'app-employee-details',
@@ -7,9 +10,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmployeeDetailsComponent implements OnInit {
 
-  constructor() { }
+  id!: number;
+  employee !: Employee
+  constructor(private route: ActivatedRoute, private employeService: EmployeeService) { }
 
   ngOnInit(): void {
+    this.id = this.route.snapshot.params['id'];
+
+    this.employee = new Employee();
+    this.employeService.getEmployeeById(this.id).subscribe( data => {
+      this.employee = data;
+    });
   }
 
 }
