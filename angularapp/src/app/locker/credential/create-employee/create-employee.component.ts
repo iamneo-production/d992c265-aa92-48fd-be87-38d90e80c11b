0e2d980e72f22fff01274b03dcaf9e86956be272
Rajendhiran27 <81033046+Rajendhiran27@users.noreply.github.com>
2022-04-07 16:33:29 +0530

    Update create-employee.component.ts

--- angularapp/src/app/locker/credential/create-employee/create-employee.component.ts
+++ angularapp/src/app/locker/credential/create-employee/create-employee.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Employee } from '../employee';
+import { EmployeeService } from '../employee.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create-employee',
@@ -6,10 +9,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-employee.component.css']
 })
 export class CreateEmployeeComponent implements OnInit {
-
-  constructor() { }
+hide=true;
+  employee: Employee = new Employee();
+  constructor(private employeeService: EmployeeService,
+    private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  saveEmployee(){
+    this.employeeService.createEmployee(this.employee).subscribe( data =>{
+      console.log(data);
+      this.goToEmployeeList();
+    },
+    error => console.log(error));
+  }
+
+  goToEmployeeList(){
+    this.router.navigate(['/employees']);
+  }
+  
+  onSubmit(){
+    console.log(this.employee);
+    this.saveEmployee();
+  }
 }
