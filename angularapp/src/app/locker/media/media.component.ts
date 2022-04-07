d818456e37e7653634627d6a430b9e08e6190422
Rajendhiran27 <81033046+Rajendhiran27@users.noreply.github.com>
2022-04-07 16:30:25 +0530

    Create media.component.ts

---
+++ angularapp/src/app/locker/media/media.component.ts
@@ -0,0 +1,68 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpEventType } from '@angular/common/http';
+@Component({
+  selector: 'app-media',
+  templateUrl: './media.component.html',
+  styleUrls: ['./media.component.css']
+})
+export class MediaComponent implements OnInit {
+
+  constructor(private httpClient: HttpClient) { }
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  selectedFile!: File;
+  retrievedImage: any;
+  base64Data: any;
+  retrieveResonse: any;
+  message!: string;
+  imageName: any;
+
+  //Gets called when the user selects an image
+  public onFileChanged(event:any) {
+    //Select File
+    this.selectedFile = event.target.files[0];
+  }
+
+
+  //Gets called when the user clicks on submit to upload the image
+  onUpload() {
+    console.log(this.selectedFile);
+    
+    //FormData API provides methods and properties to allow us easily prepare form data to be sent with POST HTTP requests.
+    const uploadImageData = new FormData();
+    uploadImageData.append('imageFile', this.selectedFile, this.selectedFile.name);
+  
+    //Make a call to the Spring Boot Application to save the image
+    this.httpClient.post('https://8080-acdebacddedfbbeedadebbaacaca.examlyiopb.examly.io/image/upload', uploadImageData, { observe: 'response' })
+      .subscribe((response) => {
+        if (response.status === 200) {
+          this.message = 'Image uploaded successfully';
+        } else {
+          this.message = 'Image not uploaded successfully';
+        }
+      }
+      );
+
+
+  }
+
+    //Gets called when the user clicks on retieve image button to get the image from back end
+    getImage() {
+    //Make a call to Sprinf Boot to get the Image Bytes.
+    this.httpClient.get('https://8080-acdebacddedfbbeedadebbaacaca.examlyiopb.examly.io/image/get/' + this.imageName)
+      .subscribe(
+        res => {
+          this.retrieveResonse = res;
+          this.base64Data = this.retrieveResonse.picByte;
+          this.retrievedImage = 'data:image/jpeg;base64,' + this.base64Data;
+        }
+      );
+  }
+}
+
+//   ngOnInit(): void {
+//   }
+
+// }
