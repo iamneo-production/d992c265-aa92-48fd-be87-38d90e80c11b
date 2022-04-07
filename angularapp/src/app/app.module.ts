9ac986faee06812844a0b6816ac7723ccb697e48
Rajendhiran27 <81033046+Rajendhiran27@users.noreply.github.com>
2022-04-07 18:26:32 +0530

    Update app.module.ts

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -1,65 +1,69 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FlexLayoutModule } from '@angular/flex-layout';
+import { LoginComponent } from './login/login.component';
+import {FlexLayoutModule} from '@angular/flex-layout';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {MatButtonModule} from'@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
-import { HttpClientModule } from '@angular/common/http';
-import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
-import { FormsModule } from '@angular/forms';
-import { AdminComponent } from './admin/admin.component';
 import { HomepageComponent } from './homepage/homepage.component';
+import {MatExpansionModule} from '@angular/material/expansion';
+import { EmployeeListComponent } from './locker/credential/employee-list/employee-list.component';
+import { CreateEmployeeComponent } from './locker/credential/create-employee/create-employee.component';
+import { UpdateEmployeeComponent } from './locker/credential/update-employee/update-employee.component';
+import { EmployeeDetailsComponent } from './locker/credential/employee-details/employee-details.component';
 import { CreateBankComponent } from './locker/Bank-Locker/create-bank/create-bank.component';
 import { DetailsBankComponent } from './locker/Bank-Locker/details-bank/details-bank.component';
 import { ListBankComponent } from './locker/Bank-Locker/list-bank/list-bank.component';
 import { UpdateBankComponent } from './locker/Bank-Locker/update-bank/update-bank.component';
-import { CreateEmployeeComponent } from './locker/credential/create-employee/create-employee.component';
-import { EmployeeDetailsComponent } from './locker/credential/employee-details/employee-details.component';
+import { MediaComponent } from './locker/media/media.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { AdminComponent } from './admin/admin.component';
+import {MatTabsModule} from '@angular/material/tabs';
+import { AdminloginComponent } from './admin/adminlogin/adminlogin.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     RegisterComponent,
-    AdminComponent,
     HomepageComponent,
+    EmployeeListComponent,
+    EmployeeDetailsComponent,
+    CreateEmployeeComponent,
+    UpdateEmployeeComponent,
     CreateBankComponent,
     DetailsBankComponent,
     ListBankComponent,
     UpdateBankComponent,
-    CreateEmployeeComponent,
-    EmployeeDetailsComponent,
-    // LoginComponent,
-    // RegisterComponent,
-    // AdminComponent,
-    // AdminloginComponent,
-    // HomepageComponent,
-    // BankLockerComponent,
-    // CreateBankComponent
+    MediaComponent,
+    AdminComponent,
+    AdminloginComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    FlexLayoutModule,
     MatFormFieldModule,
     MatInputModule,
     MatButtonModule,
     MatCardModule,
     MatToolbarModule,
     MatIconModule,
-    FlexLayoutModule,
+    MatExpansionModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    MatTabsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
-// rm -rf node_modules
