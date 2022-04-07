69dba013a4890d96ed4f0f5e82221f48c31da295
Rajendhiran27 <81033046+Rajendhiran27@users.noreply.github.com>
2022-04-07 18:52:48 +0530

    Create Register.java

---
+++ springapp/src/main/java/com/examly/springapp/model/Register.java
@@ -0,0 +1,74 @@
+package com.examly.springapp.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "user")
+public class Register{
+    	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+
+    @Column(name = "confirm_password")
+    private String confirmPassword;
+
+    @Column(name = "email")
+    private String emailId;
+    @Column(name = "password")
+    private String password;
+    @Column(name = "phone_number")
+    private String phoneNumber;
+    @Column(name = "user_name")
+    private String userName;
+
+    public Register(){
+
+    }
+    public Register( String userName, String password, String confirmPassword, String phoneNumber,           String emailId) {
+                super();
+        this.userName = userName;
+       this.password = password;
+       this. confirmPassword = confirmPassword;
+       this. phoneNumber = phoneNumber;
+        this.emailId = emailId;
+    }
+    public String getUserName() {
+        return userName;
+    }
+    public void setUserName(String userName) {
+       this.userName = userName;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+    public String getEmailId() {
+        return emailId;
+    }
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+
+
+}
