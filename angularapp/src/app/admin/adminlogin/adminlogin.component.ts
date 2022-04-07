db64026f9a319c58707baca0d428689941264cae
Rajendhiran27 <81033046+Rajendhiran27@users.noreply.github.com>
2022-04-07 18:33:15 +0530

    Create adminlogin.component.ts

---
+++ angularapp/src/app/admin/adminlogin/adminlogin.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-adminlogin',
+  templateUrl: './adminlogin.component.html',
+  styleUrls: ['./adminlogin.component.css']
+})
+export class AdminloginComponent implements OnInit {
+hide=true;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
